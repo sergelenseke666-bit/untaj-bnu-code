c2f12bdf6b1226ae971775b0830dd8b0eed250fc
sergelenseke666-bit <sergelenseke666@gmail.com>
2026-05-23 10:43:37 +0800

    are you sleeping song

--- main.py
+++ main.py
@@ -28,29 +28,29 @@ def on_button_pressed_a():
     music.play(music.tone_playable(392, music.beat(BeatFraction.WHOLE)),
         music.PlaybackMode.UNTIL_DONE)
     music.rest(music.beat(BeatFraction.WHOLE))
-    music.play(music.tone_playable(392, music.beat(BeatFraction.WHOLE)),
+    music.play(music.tone_playable(392, music.beat(BeatFraction.HALF)),
         music.PlaybackMode.UNTIL_DONE)
-    music.play(music.tone_playable(440, music.beat(BeatFraction.WHOLE)),
+    music.play(music.tone_playable(440, music.beat(BeatFraction.HALF)),
         music.PlaybackMode.UNTIL_DONE)
-    music.play(music.tone_playable(392, music.beat(BeatFraction.WHOLE)),
+    music.play(music.tone_playable(392, music.beat(BeatFraction.HALF)),
         music.PlaybackMode.UNTIL_DONE)
-    music.play(music.tone_playable(349, music.beat(BeatFraction.WHOLE)),
+    music.play(music.tone_playable(349, music.beat(BeatFraction.HALF)),
         music.PlaybackMode.UNTIL_DONE)
-    music.play(music.tone_playable(330, music.beat(BeatFraction.WHOLE)),
+    music.play(music.tone_playable(330, music.beat(BeatFraction.HALF)),
         music.PlaybackMode.UNTIL_DONE)
     music.rest(music.beat(BeatFraction.WHOLE))
     music.play(music.tone_playable(262, music.beat(BeatFraction.WHOLE)),
         music.PlaybackMode.UNTIL_DONE)
     music.rest(music.beat(BeatFraction.WHOLE))
-    music.play(music.tone_playable(392, music.beat(BeatFraction.WHOLE)),
+    music.play(music.tone_playable(392, music.beat(BeatFraction.HALF)),
         music.PlaybackMode.UNTIL_DONE)
-    music.play(music.tone_playable(440, music.beat(BeatFraction.WHOLE)),
+    music.play(music.tone_playable(440, music.beat(BeatFraction.HALF)),
         music.PlaybackMode.UNTIL_DONE)
-    music.play(music.tone_playable(392, music.beat(BeatFraction.WHOLE)),
+    music.play(music.tone_playable(392, music.beat(BeatFraction.HALF)),
         music.PlaybackMode.UNTIL_DONE)
-    music.play(music.tone_playable(349, music.beat(BeatFraction.WHOLE)),
+    music.play(music.tone_playable(349, music.beat(BeatFraction.HALF)),
         music.PlaybackMode.UNTIL_DONE)
-    music.play(music.tone_playable(330, music.beat(BeatFraction.WHOLE)),
+    music.play(music.tone_playable(330, music.beat(BeatFraction.HALF)),
         music.PlaybackMode.UNTIL_DONE)
     music.rest(music.beat(BeatFraction.WHOLE))
     music.play(music.tone_playable(262, music.beat(BeatFraction.WHOLE)),
